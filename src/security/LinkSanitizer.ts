9c4e9581316d209bcfb39164ee83444589a13ef9
Mahdi Yari <m.yary16@gmail.com>
2020-05-05 04:56:47 +0430

    Update LinkSanitizer.ts

--- src/security/LinkSanitizer.ts
+++ src/security/LinkSanitizer.ts
@@ -3,17 +3,17 @@ import ow from "ow";
 import { Log } from "../Log";
 
 import { Phishing } from "./Phishing";
-import { URL } from "url";
+import * as URL from "url";
 
 export class LinkSanitizer {
     private options: LinkSanitizer.Options;
-    private baseUrl: URL;
+    private baseUrl: URL.URL;
     private topLevelsBaseDomain: string;
 
     public constructor(options: LinkSanitizer.Options) {
         LinkSanitizer.Options.validate(options);
         this.options = options;
-        this.baseUrl = new URL(this.options.baseUrl);
+        this.baseUrl = new URL.URL(this.options.baseUrl);
         this.topLevelsBaseDomain = this.getTopLevelBaseDomainFromBaseUrl(this.baseUrl);
     }
 
